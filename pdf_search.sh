45ad3d40b70ed686b494e240db0728a423b9e5bb
rupert@piranha <rupertcollins@gmail.com>
2014-02-19 22:05:27 -0400

    added code to search multiple pdfs

--- pdf_search.sh
+++ pdf_search.sh
@@ -2,3 +2,5 @@
 #greps text from pdf without writing to file
 pdftotext CLOFFSCA.pdf - | grep 'Nhamund'
 pdftotext ferraris2007_coc.pdf - | grep 'Nhamund'
+#to search multiple files
+find /home/rupert/Journals_OLD/ichthyology_papers/Ictiologia\ por\ autores -name '*.pdf' -exec sh -c 'pdftotext -q "{}" - | grep --with-filename --label="{}" --color "Nhamund"' \;
