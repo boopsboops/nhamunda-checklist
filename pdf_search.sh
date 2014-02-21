0dac9b5d4d0ae6045976a8a3d38b059f4f8dcc5b
rupert@piranha <rupertcollins@gmail.com>
2014-02-20 20:16:53 -0400

    changed pdf search

--- pdf_search.sh
+++ pdf_search.sh
@@ -3,4 +3,4 @@
 pdftotext CLOFFSCA.pdf - | grep 'Nhamund'
 pdftotext ferraris2007_coc.pdf - | grep 'Nhamund'
 #to search multiple files
-find /home/rupert/Journals_OLD/ichthyology_papers/Ictiologia\ por\ autores -name '*.pdf' -exec sh -c 'pdftotext -q "{}" - | grep --with-filename --label="{}" --color "Nhamund"' \;
+find /home/rupert/Journals_OLD/ichthyology_papers -name '*.pdf' -exec sh -c 'pdftotext -q "{}" - | grep --with-filename --label="{}" --color "Nhamund"' \;
