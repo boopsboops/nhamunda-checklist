44efec426b441c5ea44dda19a35ca41c4bb2ad7f
rupert@jau <rupertcollins@gmail.com>
2014-03-28 01:39:20 -0400

    finished sample sites map

--- ggmap.R
+++ ggmap.R
@@ -1,21 +1,19 @@
+# load ggmap and ggplot
 require(ggmap)
 
-mapImageData1 <- get_map(location = c(lon = -57.36856, lat = -1.71782),  color = "color",    source = "google",    maptype = "terrain",    zoom = 10)
+# read gps data
+gps <- read.table(file="/home/rupert/LaTeX/nhamunda-checklist/site_gps.csv", sep=",", header=TRUE, stringsAsFactors=FALSE)
 
-m <- ggmap(mapImageData1, extent = "panel", ylab = "Latitude", xlab = "Longitude") 
-m + geom_point(aes(x = deg_long, y = deg_lat), colour = "blue", size=10, data = gps)
+# get a base map from google
+mapImageData1 <- get_map(location=c(lon=-57.05, lat=-1.99), color="color", source="google", maptype="terrain", zoom=10)
+# create map
+m <- ggmap(mapImageData1, extent="panel") 
+# plot, and add points and text
+m + geom_point(data=gps, aes(x=deg_long, y=deg_lat), colour="black", shape=21, bg="orange", size=5, alpha=0.55, na.rm=TRUE) + geom_text(data=gps, aes(x=deg_long, y=deg_lat, label=site_code), colour="grey20", size=3, fontface=2, vjust=-1, hjust=-0.1, na.rm=TRUE) + labs(x=NULL, y=NULL) 
+#save
+ggsave(file="testmap.png", width=8, height=8, dpi = 150)
 
 ?geom_point
-
-aes=(na.omit(gps)[,2], na.omit(gps)[,3])
-gps <- read.table(file="/home/rupert/LaTeX/nhamunda-checklist/site_gps.csv", sep=",", header=TRUE)
-
-gps <- na.omit(gps)
-
-
 ?get_map
 ?ggmap
-
-go <- geocode("faro")
-
-df <- round(data.frame(x = jitter(rep(-95.36, 50), amount = .3), y = jitter(rep( 29.76, 50), amount = .3)), digits = 2)
+?geom_text
